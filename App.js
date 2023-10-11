9e41f611a0ebbf0137c686797d47bb5856331a67
Siddharth Borman <siddharth02b@gmail.com>
2023-10-11 13:19:57 +0530

    loginpage added

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex-1 bg-blue-200 justify-center items-center">
+      <View style={{height: hp(45), width: wp(80)}} className = "bg-white flex items-center justify-center rounded-3xl">
+        {/*email container*/}
+        <View className="">
+          <View style={{width:wp(65)}} className="border-2 border-blue-900 rounded-xl">
+            <Text className="text-blue-200 p-4">email</Text>
+          </View>
+        </View>
+
+        {/* password container */}
+        <View className="pt-2">
+          <View style={{width:wp(65)}} className="border-2 border-blue-900 rounded-xl">
+            <Text className="text-blue-200 p-4">password</Text>
+          </View>
+        </View>
+
+        {/* login */}
+        <View className="pt-8">
+          <TouchableOpacity style={{width:wp(65)}} className="bg-blue-900 rounded-3xl flex items-center">
+            <Text className="font-bold text-white p-4">Sign in</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View className="pt-8">
+          <Text>Not having an account?</Text>
+        </View>
+
+        <View className ="pt-2">
+          <TouchableOpacity>
+            <Text className="text-blue-200 font-bold">SIGN UP</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.{js,jsx,ts,tsx}", "./src/screens/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    // colors:{
+    //     "white" : "#ffffff",
+    //     "main" : "#001F4E",
+    //     "lite" : "#779FE5",
+    // },
+  },
+  plugins: [],
+}
+
